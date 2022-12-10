c1fd5a0e07c5329caca47549c5f9d532f40e03c4
Ekaterina <87811449+GalraKim@users.noreply.github.com>
2022-12-10 23:51:29 +0700

    Task1

---
+++ MethodSquare_SHEA.cpp
@@ -0,0 +1,123 @@
+﻿/Метод квадратного корня для СЛАУ/
+#include <iostream>
+#include <fstream>
+using namespace std;
+void OutMatrix(double* arr[], int n){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout.width(10);
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+void OutMatrixOneD(double arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout <<"x["<<i+1<<"]= "<< arr[i] << endl;
+    }
+    cout << endl;
+}
+double* Cholesky(double* array[], int m, double b[]) {
+    double** triangleArr = new double* [m];
+    for (int i = 0; i < m; i++) {
+        triangleArr[i] = new double[m];
+        for (int j = 0; j < m; j++) {
+            triangleArr[i][j] = 0;
+        }
+    }
+    triangleArr[0][0] = sqrt(array[0][0]);
+    for (int j = 1; j < m; j++) {
+        triangleArr[0][j] = (array[0][j]) / triangleArr[0][0];
+    }
+    
+    for (int j = 1; j < m; j++) {
+        for (int i = 1; i < m; i++) {
+            double sum = 0.0;
+            double secSum = 0.0;
+            for (int k = 0; k < i; k++) {
+                sum = sum + triangleArr[k][i] * triangleArr[k][i];
+                if (i<j) secSum = secSum + triangleArr[k][i] * triangleArr[k][j];
+            }
+            triangleArr[i][i] = sqrt(array[i][i] - sum);
+            if (i < j) triangleArr[i][j] = (array[i][j] - secSum) / triangleArr[i][i];
+        }
+    }
+
+    double* result = new double[m];
+    double* matrixY = new double[m];
+    //ищем столбец У
+    matrixY[0] = b[0] / triangleArr[0][0];
+    for (int i = 1; i < m; i++) {
+        double sum = 0.0;
+        for (int k = 0; k < i; k++) {
+            sum = sum + triangleArr[k][i] * matrixY[k];
+        }
+        matrixY[i] = (b[i] - sum) / triangleArr[i][i];
+    }
+    // Наконец то находим столбец иксов
+    result[m-1] = (matrixY[m-1]) / triangleArr[m-1][m-1];
+    for (int i = m-2;i>=0;i--) {
+        double sum = 0.0;
+        for (int k = i+1; k <= m-1; k++) {
+            sum = sum + triangleArr[i][k] * result[k];
+        }
+        result[i] = (matrixY[i] - sum) / triangleArr[i][i];
+    }
+    return result;
+}
+double** ReverseMatrix(double* array[], int m) {
+    double** result = new double* [m];
+    for (int i = 0; i < m; i++) {
+        double* tempB = new double[m];
+        for (int k = 0; k < m; k++) {
+            if (k == i) tempB[k] = 1;
+            else tempB[k] = 0;
+        }
+        result[i] = Cholesky(array, m, tempB);
+    }
+    return result;
+}
+int main()
+{
+    string path = "Matrix.txt";
+    ifstream fin;
+    fin.open(path);
+    int rows, cols;
+
+        fin >> rows;
+        cols = rows;
+
+        double** arr = new double* [rows];
+        double* vectorX = new double [rows];
+        double* vector = new double[rows];
+        double** reversedArr = new double* [rows];
+        for (int i = 0; i < rows; i++) {
+            arr[i] = new double[cols];
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+            {
+                fin >> arr[i][j];
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            fin >> vectorX[i];
+        }
+        vector = Cholesky(arr, rows, vectorX);
+        cout << "Found roots of the systems of equation: " << endl;
+        OutMatrixOneD(vector, rows);
+        cout << "Reversed matrix A: " << endl;
+        reversedArr = ReverseMatrix(arr, rows);
+        OutMatrix(reversedArr, rows);
+
+    delete[] vectorX;
+    delete[] vector;
+    for (int i = 0; i < rows; i++) {
+        delete[] arr[i];
+        delete[] reversedArr[i];
+    }
+    delete[] arr;
+    delete[] reversedArr;
+}
+
+
---
+++ SweepMethod2SHEA.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+void OutMatrix(long double* arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout.width(10);
+            
+            cout << setprecision(3) << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+void OutMatrixOneD(long double arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "x[" << i + 1 << "]= " << setprecision(3) << arr[i] << endl;
+    }
+    cout << endl;
+}
+void sweep1(double* matrix[], double freeMembers[], int size) {
+    double temp;
+    double* a = new double[size];
+    double* b = new double[size];
+    double* x = new double[size];
+    temp = matrix[0][0];
+    a[0] = -matrix[0][1] / temp;
+    b[0] = freeMembers[0] / temp;
+    for (int i = 1; i < size - 1; i++) {
+        temp = matrix[i][i] + matrix[i][i - 1] * a[i - 1];
+        a[i] = -matrix[i][i + 1] / temp;
+        b[i] = (freeMembers[i] - matrix[i][i - 1] * b[i - 1]) / temp;
+    }
+    x[size - 1] = (freeMembers[size - 1] - matrix[size - 1][size - 2] * b[size - 2]) / (matrix[size - 1][size - 1] + matrix[size - 1][size - 2] * a[size - 2]);
+    for (int i = size - 2; i >= 0; i--) {
+        x[i] = a[i] * x[i + 1] + b[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout.width(10);
+        cout << x[i] << "\t";
+    }
+    delete[] a, b, x;
+}
+
+long double* Sweep(long double* arr[], int rows, long double B[]) {
+    long double* r = new long double[rows];
+    long double* t = new long double[rows];
+    long double* x = new long double[rows];
+    long double a0 = 0.0, cn = 0.0;
+    r[rows - 1] = (arr[rows-1 ][rows - 2] / arr[rows - 1][rows - 1]);
+    t[rows - 1] = (B[rows-1] / arr[rows - 1][rows-1]);
+    for (int i = rows - 2; i >=0; i--) {
+        if (i == 0) r[i] =( a0 / (arr[i][i] - arr[i][i+1] * r[i + 1]));
+        else r[i] = ((arr[i][i - 1]) / (arr[i][i] - arr[i][i+1] * r[i + 1]));
+        t[i] = ((B[i] + arr[i][i + 1] * t[i + 1]) / (arr[i][i] - arr[i][i+1] * r[i + 1]));
+    }
+    
+    x[0] = (B[0] + arr[0][1] * t[0]) / (arr[0][0] - arr[0][1] * r[0]);
+    for (int i = 1; i < rows; i++) {
+        x[i] = r[i] * x[i - 1] + t[i];
+    }
+    OutMatrixOneD(r, rows);
+    OutMatrixOneD(t, rows);
+    return x;
+}
+
+int main()
+{
+    string path = "Matrix.txt";
+    ifstream fin;
+    fin.open(path);
+    int rows, cols;
+
+    fin >> rows;
+    cols = rows;
+
+    long double** arr = new long double* [rows];
+    long double* vectorX = new long double[rows];
+    double* vector = new double[rows];
+    double** reversedArr = new double* [rows];
+    for (int i = 0; i < rows; i++) {
+        arr[i] = new long double[cols];
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+        {
+            fin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        fin >> vectorX[i];
+    }
+    cout << "Found roots of the systems of equation: " << endl;
+    OutMatrixOneD(Sweep(arr,rows,vectorX), rows);
+    delete[] arr;
+    delete[] vectorX;
+
+}
+
+
