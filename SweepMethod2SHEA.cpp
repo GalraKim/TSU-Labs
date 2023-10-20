55ce0af60b82f5be8c7d97d0a224d737f86755b8
Ekaterina <87811449+GalraKim@users.noreply.github.com>
2023-10-20 12:42:17 +0700

    Update SweepMethod2SHEA.cpp

--- SweepMethod2SHEA.cpp
+++ SweepMethod2SHEA.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+
+#include <iostream>
 #include <fstream>
 #include <iomanip>
 using namespace std;
@@ -6,7 +7,7 @@ void OutMatrix(long double* arr[], int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout.width(10);
-            
+
             cout << setprecision(3) << arr[i][j] << "  ";
         }
         cout << endl;
@@ -18,51 +19,34 @@ void OutMatrixOneD(long double arr[], int n) {
     }
     cout << endl;
 }
-void sweep1(double* matrix[], double freeMembers[], int size) {
-    double temp;
-    double* a = new double[size];
-    double* b = new double[size];
-    double* x = new double[size];
-    temp = matrix[0][0];
-    a[0] = -matrix[0][1] / temp;
-    b[0] = freeMembers[0] / temp;
-    for (int i = 1; i < size - 1; i++) {
-        temp = matrix[i][i] + matrix[i][i - 1] * a[i - 1];
-        a[i] = -matrix[i][i + 1] / temp;
-        b[i] = (freeMembers[i] - matrix[i][i - 1] * b[i - 1]) / temp;
-    }
-    x[size - 1] = (freeMembers[size - 1] - matrix[size - 1][size - 2] * b[size - 2]) / (matrix[size - 1][size - 1] + matrix[size - 1][size - 2] * a[size - 2]);
-    for (int i = size - 2; i >= 0; i--) {
-        x[i] = a[i] * x[i + 1] + b[i];
-    }
+int ProveStable(long double A[], long double B[], long double C[], int n) {
+    int flag = 1;
+    for (int i = 0; i < n; i++) {
+        if (abs(B[i]) < abs(A[i]) + abs(C[i])) {
+            cout << "The method is not stable" << endl;
+            return 0;
+        }
+        else  if (abs(B[i]) > abs(A[i]) + abs(C[i])) flag = 0;//нашли одно строгое неравенство
 
-    for (int i = 0; i < size; i++)
-    {
-        cout.width(10);
-        cout << x[i] << "\t";
     }
-    delete[] a, b, x;
+    if (flag==1) cout << "The method is not stable" << endl;
+    else cout << "The method is stable" << endl;
 }
-
-long double* Sweep(long double* arr[], int rows, long double B[]) {
-    long double* r = new long double[rows];
-    long double* t = new long double[rows];
-    long double* x = new long double[rows];
-    long double a0 = 0.0, cn = 0.0;
-    r[rows - 1] = (arr[rows-1 ][rows - 2] / arr[rows - 1][rows - 1]);
-    t[rows - 1] = (B[rows-1] / arr[rows - 1][rows-1]);
-    for (int i = rows - 2; i >=0; i--) {
-        if (i == 0) r[i] =( a0 / (arr[i][i] - arr[i][i+1] * r[i + 1]));
-        else r[i] = ((arr[i][i - 1]) / (arr[i][i] - arr[i][i+1] * r[i + 1]));
-        t[i] = ((B[i] + arr[i][i + 1] * t[i + 1]) / (arr[i][i] - arr[i][i+1] * r[i + 1]));
+long double* Sweep(long double A[], long double B[], long double C[], long double F[], int n) {
+    long double* R = new long double[n + 1];
+    long double* T = new long double[n + 1];
+    long double* x = new long double[n + 1];
+    long double an = -A[n-1], a = -A[1], b = B[1], bn = B[n-1], c0 = -C[0], c = -C[1], f0 = F[0], fn = F[n-1], f = F[1], b0 = B[0];    
+    R[n] = an / bn;
+    T[n] = fn / bn;
+    for (int i = n - 1; i > 0; i--) {
+        R[i] = a / (b - c * R[i + 1]);
+        T[i] = (f + c * T[i + 1]) / (b - c * R[i + 1]);
     }
-    
-    x[0] = (B[0] + arr[0][1] * t[0]) / (arr[0][0] - arr[0][1] * r[0]);
-    for (int i = 1; i < rows; i++) {
-        x[i] = r[i] * x[i - 1] + t[i];
+    x[0] = (f0 + c0 * T[1]) / (b0 - c0 * R[1]); 
+    for (int i = 1; i <= n; i++) {
+        x[i] = R[i] * x[i - 1] + T[i];
     }
-    OutMatrixOneD(r, rows);
-    OutMatrixOneD(t, rows);
     return x;
 }
 
@@ -71,30 +55,28 @@ int main()
     string path = "Matrix.txt";
     ifstream fin;
     fin.open(path);
-    int rows, cols;
-
+    int rows;
     fin >> rows;
-    cols = rows;
-
-    long double** arr = new long double* [rows];
+    long double* vectorA = new long double[rows];
+    long double* vectorB = new long double[rows];
+    long double* vectorC = new long double[rows];
     long double* vectorX = new long double[rows];
-    double* vector = new double[rows];
-    double** reversedArr = new double* [rows];
     for (int i = 0; i < rows; i++) {
-        arr[i] = new long double[cols];
+        fin >> vectorA[i];
     }
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++)
-        {
-            fin >> arr[i][j];
-        }
+        fin >> vectorB[i];
+    }
+    for (int i = 0; i < rows; i++) {
+        fin >> vectorC[i];
     }
     for (int i = 0; i < rows; i++) {
         fin >> vectorX[i];
     }
+    ProveStable(vectorA, vectorB, vectorC, rows);
+
     cout << "Found roots of the systems of equation: " << endl;
-    OutMatrixOneD(Sweep(arr,rows,vectorX), rows);
-    delete[] arr;
+    OutMatrixOneD(Sweep(vectorA, vectorB, vectorC, vectorX, rows), rows);
     delete[] vectorX;
 
 }
